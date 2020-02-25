be02c5f2a185655c23443e03b8f0b4bac3a7770a
Brieana Molinari <brieanamolinari@tgmail.com>
2020-02-24 22:43:13 -0700

    updating all files

--- Assignment1/door.py
+++ Assignment1/door.py
@@ -2,8 +2,7 @@ from lock import Lock
 from handle import Handle
 
 class Door:
-    def __init__(self, woodType : str, height : float):
-         self._lock :
+    def __init__(self, woodType : str, height : float, diameter = Lock.DEFAULT_DIAMETER, state = Lock.DEFAULT_STATE):
          self._woodType : str = woodType
          self.height : float
 
