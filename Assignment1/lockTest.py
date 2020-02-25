9d62bd65b9ca82e450f4cb60b10a6eeb8dccca2b
Brieana Molinari <brieanamolinari@tgmail.com>
2020-02-24 23:00:20 -0700

    finishing tests for lock.py

--- Assignment1/lockTest.py
+++ Assignment1/lockTest.py
@@ -10,7 +10,6 @@ class LockTest(unittest.TestCase):
         self.assertEqual(lock._diameter, Lock.DEFAULT_DIAMETER)
         self.assertEqual(lock._needKey, False) # difference between lock._diameter and lock.diameter?
 
-
     def testSpecific(self):
         testDiameter : float = 1.15
         testState : bool  = True
@@ -18,6 +17,16 @@ class LockTest(unittest.TestCase):
         self.assertEqual(lock.diameter, testDiameter)
         self.assertEqual(lock.state, testState)
         self.assertEqual(lock._needKey, True)
+
+    def testStateLocked(self): 
+        testState : bool = True
+        lock = Lock(state = testState, diameter = Lock.DEFAULT_DIAMETER)
+        self.assertEqual(lock._needKey, True)
+
+    def testStateUnlocked(self):
+        testState : bool = False
+        lock = Lock(state = testState, diameter = Lock.DEFAULT_DIAMETER)
+        self.assertEqual(lock._needKey, False)
 
 
     if __name__ == '__main__':
