b5556be9407c3a6215298208fc657daf1c2e538c
Brieana Molinari <brieanamolinari@tgmail.com>
2020-05-12 16:09:57 -0600

    starting to build scheduling objects week and day

--- oopFinal/categories.py
+++ oopFinal/categories.py
@@ -3,6 +3,7 @@ from exercise import Exercise
 #Each targeted muscle group has a class which is a child class to the parent class Exercise
 
 class Legs(Exercise):
+    num_of_leg_exercises = 0
     DEFAULT_REPS : int = 20
     DEFAULT_WEIGHT : int = 0
     raise_reps = 20
@@ -12,14 +13,16 @@ class Legs(Exercise):
         super().__init__(name, aerobic, category)
         self._reps = reps
         self._weight = weight
+
+        Legs.num_of_leg_exercises += 1
     
     @property 
-    def reps(self) -> int:
-        return self._reps
+    def reps(self) -> str:
+        return '{} reps'.format(self._reps)
 
     @property
-    def weight(self) -> int:
-        return self._weight
+    def weight(self) -> str:
+        return '{} lbs'.format(self._weight)
 
     @reps.setter
     def reps(self, value : int) -> None:
@@ -41,6 +44,7 @@ class Legs(Exercise):
     
 
 class Arms(Exercise):
+    num_of_arm_exercises = 0
     DEFAULT_REPS : int = 20
     DEFAULT_WEIGHT : int = 0
     raise_reps : int = 10 
@@ -51,14 +55,16 @@ class Arms(Exercise):
         self._reps = reps
         self._weight = weight
 
+        Arms.num_of_arm_exercises += 1
+
     
     @property 
-    def reps(self) -> int:
-        return self._reps
+    def reps(self) -> str:
+        return '{} reps'.format(self._reps)
 
     @property
-    def weight(self) -> int:
-        return self._weight
+    def weight(self) -> str:
+        return '{} lbs'.format(self._weight)
 
     @reps.setter
     def reps(self, value : int) -> None:
@@ -81,6 +87,7 @@ class Arms(Exercise):
 
 
 class Abs(Exercise):
+    num_of_ab_exercises = 0
     DEFAULT_REPS : int = 20
     DEFAULT_WEIGHT : int = 0
     raise_reps = 15
@@ -91,14 +98,16 @@ class Abs(Exercise):
         self._reps = reps
         self._weight = weight
 
+        Abs.num_of_ab_exercises += 1
+
     
     @property 
-    def reps(self) -> int:
-        return self._reps
+    def reps(self) -> str:
+        return '{} reps'.format(self._reps)
 
     @property
-    def weight(self) -> int:
-        return self._weight
+    def weight(self) -> str:
+        return '{} lbs'.format(self._weight)
 
     @reps.setter
     def reps(self, value : int) -> None:
@@ -121,6 +130,7 @@ class Abs(Exercise):
     
     
 class Back(Exercise):
+    num_of_back_exercises = 0
     DEFAULT_REPS : int = 10
     DEFAULT_WEIGHT : int = 0
     raise_reps = 5
@@ -131,14 +141,16 @@ class Back(Exercise):
         self._reps = reps
         self._weight = weight
 
+        Back.num_of_back_exercises += 1
+
     
     @property 
-    def reps(self) -> int:
-        return self._reps
+    def reps(self) -> str:
+        return '{} reps'.format(self._reps)
 
     @property
-    def weight(self) -> int:
-        return self._weight
+    def weight(self) -> str:
+        return '{} lbs'.format(self._weight)
 
     @reps.setter
     def reps(self, value : int) -> None:
@@ -160,6 +172,7 @@ class Back(Exercise):
 
 
 class Cardio(Exercise):
+    num_of_cardio_exercises = 0
     DEFAULT_INTERVAL : int = 30 #seconds
     raise_interval : int = 15 #seconds
 
@@ -167,9 +180,11 @@ class Cardio(Exercise):
         super().__init__(name, aerobic, category)
         self._interval = interval
 
+        Cardio.num_of_cardio_exercises += 1
+
     @property
-    def interval(self) -> int:
-        return self._interval
+    def interval(self) -> str:
+        return '{} seconds'.format(self._interval)
 
     @interval.setter
     def intervalSetter(self, value : int) -> None:
@@ -182,3 +197,18 @@ class Cardio(Exercise):
             self._interval += (2 * self.raise_interval)
         else: 
             pass
+
+
+
+#testing code
+
+##abs_1 = Abs('Crunches', False, 'Abs')
+##print(isinstance(abs_1, Exercise))
+##print(abs_1.reps)
+
+##abs_1.setDifficulty('Intermediate')
+##print(abs_1.weight)
+
+##print(Exercise.num_of_exercises)
+##print(Arms.num_of_arm_exercises)
+##print(Abs.num_of_ab_exercises)
--- oopFinal/scheduler.py
+++ oopFinal/scheduler.py
@@ -1,6 +1,40 @@
-import datetime
-import time
+import random
 from exercise import Exercise
 from categories import Legs, Arms, Cardio, Back, Abs
 
+muscle_group = ['Legs', 'Arms' , "Cardio", "Back", 'Abs', "Rest" , "Rest"]
 
+class Day:
+
+    def __init__(self, name : str):
+        self._name = name
+        self._category = random.choice(muscle_group)
+        muscle_group.remove(self._category) #no repetitions in the week
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def category(self) -> str:
+        return self._category
+    
+
+class Week(Day):
+
+    def __init__(self, ):
+        self.week = [] #empty list to represent the weekly schedule
+        self.week.append(Day('Sunday'))
+        self.week.append(Day('Monday'))
+        self.week.append(Day('Tuesday'))
+        self.week.append(Day('Wednesday'))
+        self.week.append(Day('Thursday'))
+        self.week.append(Day('Friday'))
+        self.week.append(Day('Saturday'))
+    
+
+
+
+
+
+workout_Brie = Week()
