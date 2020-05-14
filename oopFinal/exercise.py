0af5fc69fb90ef34bc5fb35695f6382a73c38141
Brieana Molinari <brieanamolinari@tgmail.com>
2020-05-13 23:08:43 -0600

    tests built for Exercise class

--- oopFinal/exercise.py
+++ oopFinal/exercise.py
@@ -1,11 +1,15 @@
 class Exercise:
     
     num_of_exercises = 0
+    DEFAULT_REPS : int = 10
+    DEFAULT_WEIGHT : int = 0
 
-    def __init__(self, name : str, aerobic : bool, category : str):
+    def __init__(self, name : str, aerobic : bool, category : str, reps : int = DEFAULT_REPS, weight : int = DEFAULT_WEIGHT):
         self._name = name
         self._aerobic = aerobic
         self._category = category
+        self._reps = reps
+        self._weight = weight
 
         Exercise.num_of_exercises += 1 #increments number of exercises within class each time new instance is created
 
@@ -20,6 +24,15 @@ class Exercise:
     @property
     def category(self) -> str:
         return self._category
+        
+    @property 
+    def reps(self) -> str:
+        return '{} reps'.format(self._reps)
+
+    @property
+    def weight(self) -> str:
+        return '{} lbs'.format(self._weight)
+
 
     @name.setter
     def name(self, value : str) -> None:
@@ -32,5 +45,14 @@ class Exercise:
     @category.setter
     def category(self, value : str) -> None:
         self.category = value
+
+    @reps.setter
+    def reps(self, value : int) -> None:
+        self.reps = value
+
+    @weight.setter
+    def weight(self, value : int) -> None:
+        self.weight = value
+
 
 
--- oopFinal/exercisetest.py
+++ oopFinal/exercisetest.py
@@ -3,4 +3,25 @@ import unittest
 from exercise import Exercise
 
 class ExerciseTest(unittest.TestCase):
-    pass
+    
+
+    def testDefaults(self):
+        exercise_test = Exercise("Box Jumps", False, "Legs")
+        self.assertEqual(exercise_test.reps, Exercise.DEFAULT_REPS)
+        self.assertEqual(exercise_test.weight, Exercise.DEFAULT_WEIGHT)
+
+    def testSpecifics(self):
+        test_name : str = "Pull Ups"
+        test_aerobic : bool = False
+        test_category : str = "Arms"
+        test_reps : int = 30
+        test_weight : int = 10
+
+        exercise1_test = Exercise(name = test_name, aerobic = test_aerobic, category = test_category, reps = test_reps, weight = test_weight)
+        self.assertEqual(exercise1_test.name, test_name)
+        self.assertEqual(exercise1_test.aerobic, test_aerobic)
+        self.assertEqual(exercise1_test.category, test_category)
+        self.assertEqual(exercise1_test.reps, test_reps)
+        self.assertEqual(exercise1_test.weight, test_weight)
+        
+
